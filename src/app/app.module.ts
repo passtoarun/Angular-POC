ac1b30b425b120a5d5be452fe2bf2a7dd1f2b52a
arunkumar.murugesan@rntbci.com <arunkumar.murugesan@rntbci.com>
2019-08-05 12:41:11 +0530

    Layout issue has been fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { GridPageComponent } from './grid-page/grid-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     GridPageComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
